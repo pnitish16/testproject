f899c7c0c664fb0c95973ced793585506d5fffd5
Nitish <pnitish16@gmail.com>
2016-04-20 17:40:52 +0530

    new commit

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.esds001.gitsample;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        context  = MainActivity.this;
     }
 }
